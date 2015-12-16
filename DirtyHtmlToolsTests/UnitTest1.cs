f96c59db4e65e3f33100fee155b27d192dede431
Luis Henrique Soler <blamoo@live.com>
2015-12-16 00:26:31 -0200

    Commit inicial

---
+++ DirtyHtmlToolsTests/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DirtyHtmlTools;
+
+namespace DirtyHtmlToolsTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private Lexer L;
+
+        public UnitTest1()
+        {
+            L = new Lexer();
+        }
+
+        private void throwOnError(Token[] tokens)
+       { 
+            foreach (var item in tokens)
+            {
+                switch (item.Type)
+                {
+                    case TokenType.Incomplete:
+                        throw new Exception(String.Format("tag inválida em {0}", item.position));
+                    case TokenType.InvalidInsideTag:
+                        throw new Exception(String.Format("Atributo inválido em {0}", item.position));
+               } 
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string test = "<z               >aSDasdAS<b>Z\\ZZZZZ&amp;</b>asdfasdfs\naf</z><a href=\"asdfas\ndfasdf\" asdfa=\"1\" /><plau z='\"minha irmã '>sdfasdfasdfa </plau><!--asdf-->ffffffffffffffffff&teucu;wertyu\"''   <!--oirytiu <zxcvbnm> o <> eryoti--></z>";
+            throwOnError(L.Parse(test));
+        }
+    }
+}
